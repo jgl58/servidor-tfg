917a710b7cbe8b68a0f0fe1e4bd67020ffc63601
jgl58@alu.ua.es <jgl58@alu.ua.es>
2019-03-15 19:12:06 +0100

    Cambiado registro para registrarse como cliente y como profesional

--- auth.js
+++ auth.js
@@ -12,70 +12,116 @@ app.use(bp.json());
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      database : 'tfg'
+        host: '127.0.0.1',
+        user: 'root',
+        database: 'tfg'
     }
 });
 
 
-exports.login = function(req,res){
-    
-    var nick = req.body.nick;
+exports.login = function (req, res) {
+
+    var isProfesional = req.body.profesional
+    var email = req.body.email;
     var pass = req.body.pass;
-    if(nick != "" && pass != ""){
-        knex('users').where('email',nick).where('password',pass).count('id as c').then(function(total){
-            if(total[0].c == 1){
-                res.status(200).send({
-                    "mensaje": "login ok"
-                }) 
-            }else{
-                res.status(401).send({
-                    "mensaje": "login bad"
-                }) 
+    if (email != "" && pass != "") {
+
+        if (isProfesional) {
+            knex('profesionales').where('email', email).where('password', pass).count('id as c').then(function (total) {
+                if (total[0].c == 1) {
+                    res.status(200).send({
+                        "mensaje": "login ok"
+                    })
+                } else {
+                    res.status(401).send({
+                        "mensaje": "login bad"
+                    })
+                }
+            })
+        } else {
+            knex('users').where('email', email).where('password', pass).count('id as c').then(function (total) {
+                if (total[0].c == 1) {
+                    res.status(200).send({
+                        "mensaje": "login ok"
+                    })
+                } else {
+                    res.status(401).send({
+                        "mensaje": "login bad"
+                    })
+                }
+            })
+        }
+
+       
     }
+}
+
+function existe(isProfesional, email, callback) {
+
+    if(isProfesional){
+        knex('profesionales').count('id as c').where('email', email).then(function (total) {
+
+            if (total[0].c == 1) {
+                callback(true)
+            } else {
+                callback(false)
+            }
+        })
+    }else{
+        knex('users').count('id as c').where('email', email).then(function (total) {
+
+            if (total[0].c == 1) {
+                callback(true)
+            } else {
+                callback(false)
+            }
         })
     }
-}
 
-function existe(email, callback){
-    knex('users').count('id as c').where('email',email).then(function(total){
-        
-        if(total[0].c == 1){
-            callback(true)
-        }else{
-            callback(false)
-        }
-    })
-}
-
-exports.registrar = function(req,res){
     
+}
+
+exports.registrar = function (req, res) {
+
+    var isProfesional = req.body.isProfesional;
     var email = req.body.email;
     var pass = req.body.pass;
     var nombre = req.body.nombre;
     var apellidos = req.body.apellidos;
     console.log(req.body)
-    existe(email, function(exists){
+    existe(isProfesional, email, function (exists) {
         //console.log(exists)
-        if(!exists){
+        if (!exists) {
             var data = {
                 email: email,
                 pass: pass,
                 nombre: nombre,
                 apellidos: apellidos
             }
-            
-            knex('users').insert([
-                {email: data.email, password: data.pass, nombre: nombre, apellidos: apellidos}
-            ]).then(function(f){
-                knex('users').where('email',data.email).first().then(function(query){
-                    res.setHeader('Location','/users/'+query.id);
-                    res.sendStatus(201);
+
+            if(isProfesional){
+                console.log("Registrando profesional")
+                knex('profesionales').insert([
+                    { email: data.email, password: data.pass, nombre: nombre, apellidos: apellidos }
+                ]).then(function (f) {
+                    knex('profesionales').where('email', data.email).first().then(function (query) {
+                        res.sendStatus(201);
+                    })
                 })
-            }) 
-        }else{
-            res.status(401).send({userMessage: "Usuario existente, prueba con otro nick", devMessage: ""})
+            }
+            else{
+                console.log("Registrando cliente")
+                knex('users').insert([
+                    { email: data.email, password: data.pass, nombre: nombre, apellidos: apellidos }
+                ]).then(function (f) {
+                    knex('users').where('email', data.email).first().then(function (query) {
+                        res.sendStatus(201);
+                    })
+                })
+            }
+            
+        } else {
+            res.status(401).send({ userMessage: "Usuario existente, prueba con otro nick", devMessage: "" })
         }
-}) 
+    })
 }
