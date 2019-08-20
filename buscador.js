23283f6eb01807d0d754e6a1547627727cc97cf4
unknown <jgl58@alu.ua.es>
2019-08-20 20:32:32 +0200

    Creada funcion para buscador con titulo

--- buscador.js
+++ buscador.js
@@ -18,6 +18,10 @@ var knex = require('knex')({
 exports.getOfertasProvincias = function(pet,res){
 
     var provincia = pet.params.idProvincia
+    var title = pet.query.title
+    console.log(title)
+
+    if(title == undefined){
         knex('ofertas').select('ofertas.*').innerJoin('provincias','provincias.id','=','ofertas.provincia_id')
         .where('ofertas.provincia_id',provincia).then(function(data){
             res.status(200).send({
@@ -26,7 +30,29 @@ exports.getOfertasProvincias = function(pet,res){
         }).catch((error) => {
             res.status(404).send({userMessage: "Provincia no existente", devMessage: ""})
         });
+    }else{
+        getOfertasProvinciasTitulo(pet,res)
+    }
+        
     
 }
+
+
+function getOfertasProvinciasTitulo(pet,res){
+
+    var provincia = pet.params.idProvincia
+    var title = pet.query.title
+    knex('ofertas').select('ofertas.*').innerJoin('provincias','provincias.id','=','ofertas.provincia_id')
+    .where('ofertas.titulo','like','%'+title+'%')
+    .where('ofertas.provincia_id',provincia).then(function(data){
+        res.status(200).send({
+            "ofertas": data
+        }) 
+    }).catch((error) => {
+        res.status(404).send({userMessage: "Provincia no existente", devMessage: ""})
+    });
+    
+}
+
 
 
